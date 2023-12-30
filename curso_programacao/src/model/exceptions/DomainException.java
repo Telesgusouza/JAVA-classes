bce06bc5e84242541b5083a76ae87cd1dc306611
Gustavo Teles <gustavo.teles711@gmail.com>
2023-12-30 12:12:10 -0300

    good solution

---
+++ curso_programacao/src/model/exceptions/DomainException.java
@@ -0,0 +1,14 @@
+package model.exceptions;
+
+/*
+ 
+ RuntimeException - é o tipo de exceção que o computador não obriga a tratar
+ ou seja se tiver o catch pouco importa
+ 
+ Exception - mas o Exception te obriga a tratar 
+ 
+ */
+
+public class DomainException extends Exception {
+	
+}
